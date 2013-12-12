e153d27028fe970be83a1dd88ee3462bf960f455
eujeeli <eu.jee.li@gmail.com>
2013-12-11 17:02:57 -0800

    Updated Dec. 11th

--- Final_Yujie.R
+++ Final_Yujie.R
@@ -2,53 +2,93 @@
 # when the word begins lower-case, and any other words added on are upper-cased, e.g. meanFunction, xStar.
 
 
-simulation <-function(g, lb=-100, ub=100, n, k=2){
-# input function is g(x) and D, where D the domain D=(lower,upper)
+lb = -Inf
+ub = Inf
+n= 100
+k=2
+
+ars(g=dnorm, lb=-100, ub=100, n=100, k=2)
+
+ars <-function(g, lb=-100, ub=100, n, k=2){
+# input function is g(x), lower bound and upper bound, k is the number of initial points
 # n is the number of samples to generate from g(x)
+ 
+  
+# initialize the abscissae in Tk.
+x1 = -1
+xk = 1        #x1 + 2*(k-1)
 
-h <- function (x) log((g(x))) # g will be input density
+Tk <- seq(x1,xk,length=k)
 
- # derivative function of h:
+hk <- h(Tk)
+
+if (min(hk) == -Inf) stop("log(g(x)) returns -Inf, try other initial values")
+
+dhk <- hPrime(Tk)
+
+size = 0
+## Keep sampling until we accept n values
+samples = NULL
+z=z_generate(k,Tk, lb, ub)
+
+while(size < n){
+  w = runif(1)
+
+  areas <- area(k, Tk)
+  xStar <- drawxStar(k, Tk)
+  if (w <= exp(lk(xStar, Tk))-uk(xStar, Tk)){
+    size = size+1
+    samples[size] = xStar
+  }
+  else{
+    hxStar = h(xStar)
+    hPxStar = hPrime(xStar)
+    TkOld = Tk # for debugging purpose
+    Tk = sort(c(Tk, xStar))
+    k = k + 1
+    if(w <= exp(hxStar-uk(xStar, Tk))){
+      size = size + 1
+      samples[size] = xStar
+    }
+  }
+}
+
+
+}
+
+
+##Helper functions:
+
+# Calculate h(x)
+h <- function (x) {
+  log((g(x))) }
+
+# derivative function of h(x):
 hPrime <- function(x){
   d = sqrt(.Machine$double.eps)
   return((h(x+d/2)-h(x-d/2))/d)
 }
 
-z_j = rep(NA,k-1) # initialize a vector to store values for z.
-
-# initialize the abscissae in Tk.
-x1 = -1
-xk = x1 + 2*(k-1)
-Tk <- seq(x1,xk,length=k)
-
 # z_generate is a function that returns the z vector based on equation 1.
-z_generate <- function(Tk){
- for (j in 1:(k-1)){
-  xj1=Tk[j+1]
-  xj=Tk[j]
-  z_j[j] = (h(xj1)-h(xj)-xj1*hPrime(xj1)+xj*hPrime(xj))/(hPrime(xj)-hPrime(xj1)) # x starts from 1 and z starts from 0 so need to adjust index.
-  z=c(z_j,100)
- }
- return (z)
+z_generate <- function(k,Tk, lb, ub){
+  z_j = rep(NA,k-1) # initialize a vector to store values for z.
+  z_j=(hk[-1] - hk[-k] - Tk[-1]*dhk[-1] + Tk[-k]*dhk[-k]) / (dhk[-k]-dhk[-1]) # equation 1 in the paper
+  z=c(z_j,ub)
+  return (z)
 }
-
-z=z_generate(Tk)
 
 # compute uk(x)
 uk <- function(x, Tk){
-  i = 1
-  while (z[i]<x){
-    i = i+1
-  }
-  return (h(Tk[i-1])+(x-Tk[i-1])*hPrime(Tk[i-1]))
+  i = sapply(x, function(x){which(x<=z)[1]})
+  return (hk[i]+(x-Tk[i])*dhk[i])
 }
 
-library(stats)
-
 # compute sk(x)
-sk <- function(x) { #not sure if we need the input u
-  integrand = function(y) exp(uk(y))
-  return(integrand(x)/integrate(integrand, lower=lb, upper=ub))
+sk <- function(x, Tk, lb, ub) {
+  library(stats)
+  uk <- uk(x, Tk)
+  fx <- function() exp(uk)
+  return(exp(uk)/integrate(fx(x), lower=lb, upper=ub))
 }
 
 # compute lk(x):
@@ -67,6 +107,7 @@ lk <- function(x,Tk) {
 
 
 yZ <- function(k,Tk){
+  z <- z_generate(k, Tk, lb, ub)
   yZ <- rep(NA, k+1) # corresponding y values for Tk
   yZ[1] <- hPrime(Tk[1])*z[1] + h(Tk[1]) - hPrime(Tk[1])*Tk[1]
   for (i in 1:k) {
@@ -76,80 +117,54 @@ yZ <- function(k,Tk){
     yZ[i+1] = slope*z[i+1] + (y - slope*x)
   }
   return(yZ)
+} ####Need to modify
+
+
+area <- function(k, Tk){
+  z <- z_generate(k, Tk, lb, ub)
+  yZ <-yZ(k+1, Tk)
+  area <- rep(NA, k)
+  y1 <- yZ[1:(length(yZ)-1)]
+  y2 <- yZ[2:length(yZ)]
+  x1 <- z[1:(length(z)-1)]
+  x2 <- z[2:length(z)]
+  for (i in 1:k) {
+    slope = (y2[i]- y1[i])/ (x2[i]-x1[i])
+    int = y1[i] - slope*x1[i]
+    area[i] = exp(y2[i])/slope - exp(y1[i])/slope
+  }
+  return(area)
 }
 
-#area <- function(k, Tk){
-#  yZ <-yZ(k, Tk)
-#  area <- rep(NA, k)
-#  for (i in 1:length(z)-1) {
-#   x = Tk[i]
-#   y = yZ[i]
-#   x2 = z[i+1]
-#   y2 = yZ[i+1]
-#   slope = (y2-y) / (x2-x)
-#   int = y - slope*x
-#  
-#   area[i] = exp(y2)/slope - exp(y)/slope
-#  }
-#  return(area)
-# }
 
 area <- function(k, Tk){
   tmp <- c(Tk[1], z)
-  areas <- (exp(h(Tk)-hPrime(Tk)*Tk)/hPrime(Tk))*(exp(hPrime(Tk)*tmp[2:length(tmp)])-exp(hPrime(Tk)*tmp[1:(length(tmp)-1)]))
+  areas <- (exp(hk)-dhk*hk)/dhk)*(exp(dhk*tmp[-1])-exp(dhk*tmp[-length(tmp)]))
   return(areas)
 }
 
 
-## Keep sampling until we accept n values
-size = 0
 drawxStar <- function(k, Tk){
   ## Calculate xStar, out sample candidate
-  sumArea<- sum(area(k, Tk))
-  ## Let sumA be the total area under u(x) and ratioA be the cumulative percent area of each piece under u(x)
-  pct = area(k, Tk)/sumArea
-  cumsumArea <- cumsum(pct)
+  sumArea<- sum(areas)
+  ## sumArea is the total area under u(x), and pct is the percentage of each piece of area over the total area
+  pct = areas/sumArea
+  cumsumCDF <- cumsum(pct)
+  #cumsumCDF gives the piece-wise CDF values under u(x)
   u = runif(1)
-  j = which(pct <= cumsumArea)[1]
+  j = which(pct <= cumsumCDF)[1]
   if(j > 1){
     hpj <- hPrime(Tk[j])
-    xStar <- 1/hpj*log(exp(hpj*z[j-1]) + sumArea*(u-cumsumArea[j-1])*hpj*exp(hpj*Tk[j])/exp(h(Tk[1])))
+    xStar <- 1/hpj*log(exp(hpj*z[j-1]) + sumArea*(u-cumsumCDF[j-1])*hpj*exp(hpj*Tk[j])/exp(h(Tk[1])))
   }else{
     hp1 <- hPrime(Tk[1])
     xStar <- 1/hp1*log(exp(hp1*lb) + sumArea*u*hp1*exp(hp1*Tk[1])/exp(hp1))
   }
   return(xStar)
 }
-#############################################################################################
-
-samples = NULL
-while(size < n){
-  w = runif(1)
-  xStar <- drawxStar(k, Tk)
-  if (w <= exp(lk(xStar, Tk))-uk(xStar, Tk)){
-    size = size+1
-    samples[size] = xStar
-  }
-  else{
-    hxStar = h(xStar)
-    hPxStar = hPrime(xStar)
-    Tk = sort(c(Tk, xStar))
-    k = k + 1
-    z = z_generate(Tk)
-    if(w <= exp(hxStar-uk(xStar, Tk))){
-      size = size + 1
-      samples[size] = xStar
-    }
-  }
-}
-
-
-
 
 
 ####################################################################
-
-
 
 # Log-concave checks:
 # Some densities will not work with this kind of sampling; we need to check for those.
